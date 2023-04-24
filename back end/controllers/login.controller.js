1c7b9d88a607d2c06b1208320dbaa2a52ab29b20
trunganh05071995 <nonameex2@gmail.com>
2023-04-24 10:00:58 +0700

    :rotating_light: updated vm2 Sandbox version because vulnerability

--- back end/controllers/login.controller.js
+++ back end/controllers/login.controller.js
@@ -9,37 +9,49 @@ const postLogin = async (req, res, next) => {
   try {
     const { email, password } = req.body.account;
     let userDetail = await User.findOne({
-      where: { email: email, active: "active", authType: "local", role: "client" }
+      where: {
+        email: email,
+        active: 'active',
+        authType: 'local',
+        role: 'client',
+      },
     });
     if (userDetail) {
-      let isMatchPassword = bcrypt.compareSync(password.toString(), userDetail.password)
+      let isMatchPassword = bcrypt.compareSync(
+        password.toString(),
+        userDetail.password
+      );
       if (!isMatchPassword) {
-        return next({ code: 401, msg: "Email hoặc mật khẩu không đúng" })
+        return next({ code: 401, msg: 'Email hoặc mật khẩu không đúng' });
       }
 
       // tạo mã refresh token
       const refreshToken = jwtConfig.encodedToken(
         process.env.JWT_SECRET_REFRESH_KEY,
         { email: userDetail.email, id: userDetail.id },
-        constants.JWT_REFRESH_EXPIRES_TIME,
+        constants.JWT_REFRESH_EXPIRES_TIME
       );
 
       //tạo access token
-      let token = jwtConfig.encodedToken(process.env.JWT_SECRET_KEY,
-        { email: userDetail.email, id: userDetail.id })
+      let token = jwtConfig.encodedToken(process.env.JWT_SECRET_KEY, {
+        email: userDetail.email,
+        id: userDetail.id,
+      });
 
       //nếu không duy trì đăng nhập thì giữ trạng thái sống token là session
-      const expiresIn = new Date(Date.now() + constants.COOKIE_EXPIRES_TIME);
+      const EXPIRE_IN_MILISECOND = new Date(
+        Date.now() + constants.COOKIE_EXPIRES_TIME
+      );
       // ! gửi token lưu vào cookie và chỉ đọc
 
       res.cookie('access_token', token, {
         httpOnly: true,
-        expires: expiresIn,
+        expires: EXPIRE_IN_MILISECOND,
       });
       return res.status(200).json({ refreshToken, message: 'success' });
       // }
     } else {
-      res.status(401).json({ message: "Email hoặc mật khẩu không đúng" });
+      res.status(401).json({ message: 'Email hoặc mật khẩu không đúng' });
     }
   } catch (error) {
     res.status(500).json({ message: 'Đăng nhập thất bại. Thử lại', error });
@@ -60,14 +72,15 @@ const postLoginWithGoogle = async (req, res, next) => {
     const refreshToken = jwtConfig.encodedToken(
       process.env.JWT_SECRET_REFRESH_KEY,
       { id: user.id, email: user.email, keepLogin: true },
-      constants.JWT_REFRESH_EXPIRES_TIME,
+      constants.JWT_REFRESH_EXPIRES_TIME
     );
     // //save refresh token into database
     // await AccountModel.updateOne({ _id: user._id }, { refreshToken });
 
     //create JWToken -> set header -> send client
     const token = jwtConfig.encodedToken(process.env.JWT_SECRET_KEY, {
-      id: user.id, email: user.email
+      id: user.id,
+      email: user.email,
     });
 
     // if (express().get('env') === 'production') {
@@ -83,7 +96,6 @@ const postLoginWithGoogle = async (req, res, next) => {
     res.status(200).json({ refreshToken, success: true });
     // }
   } catch (error) {
-
     return res.status(401).json({ message: 'Lỗi! Vui lòng thử lại.', error });
   }
 };
@@ -94,7 +106,6 @@ const getAuth = (req, res, next) => {
 
   return res.json({ isAuth: false });
 };
-
 
 // fn: logout
 const postLogout = async (req, res, next) => {
